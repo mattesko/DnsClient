0fb97ef835b24b3f6f1fbed53ad75f20136f78b7
Matthew Lesko <mlesko1996@gmail.com>
2018-09-30 19:37:53 -0400

    Change project name dnsClient to DnsClient

---
+++ DnsClient/src/test/java/ca/mcgill/ecse/telecom/InitializerTest.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse.telecom;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple Initializer.
+ */
+public class InitializerTest {
+    
+    @Test
+    public void testInit() {
+        assertTrue(true);
+    }
+}
