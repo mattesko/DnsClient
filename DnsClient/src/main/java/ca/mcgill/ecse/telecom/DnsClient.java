e08ba19ce34114e148af9c352532478f64f4ba84
Matthew Lesko <mlesko1996@gmail.com>
2018-10-03 19:15:04 -0400

    Parse command line arguments for options, ip, and domain name

--- DnsClient/src/main/java/ca/mcgill/ecse/telecom/DnsClient.java
+++ DnsClient/src/main/java/ca/mcgill/ecse/telecom/DnsClient.java
@@ -1,14 +1,106 @@
+/***
+ * @name            DnsClient
+ * @type            Class
+ * @author          Matthew Lesko-krleza mlesko1996@gmail.com
+ * @description     Entry point for the DnsClient command line program. 
+ *                  Performs argument parsing, directs packet building, queries DNS server requests, and directs logging
+ * ======================================================================================================================
+ * Modification Log
+ * Ver. Author                  Contact                     Modification
+ * 1.0  Matthew Lesko-Krleza    mlesko1996@gmail.com        Created the class/file. Implemented command line argument parsing.
+ */
+
 package ca.mcgill.ecse.telecom;
 
+import java.util.HashMap;
+import java.util.regex.*;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+
 public final class DnsClient {
+
+    private static final String DEFAULT_TIMEOUT = "5";
+    private static final String DEFAULT_MAX_ENTRIES = "3";
+    private static final String DEFAULT_PORT = "53";
+    private static final String DEFAULT_QUERY_TYPE = "A";
 
     private DnsClient() {}
     
     /***
-     * Starts the client
+     * Entry point for the program
      * @param args 
      */
     public static void main(String[] args) {
-        System.out.print("Yay it works!");
+        try {
+            HashMap<String, String> pArgs = parseArguments(args);
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * @description Parses command line arguments
+     * @param args The command line arguments
+     * @return Arguments parsed and sorted by their respective option and argument type
+     *         Keys are: timeout, maxEntries, port, queryType, dnsIp, domainName
+     * @throws Exception
+     */
+    public static HashMap<String, String> parseArguments(String[] args) throws Exception {
+        HashMap<String, String> optVals = new HashMap<>();
+        String timeout, maxEntries, port, queryType, dnsIp = "", domainName = "";
+
+        Options options = new Options();
+        options.addOption("t", true, "timeout");
+        options.addOption("r", true, "max-entries");
+        options.addOption("p", true, "port");
+        options.addOption("mx", false , "mail-server");
+        options.addOption("ns", false , "name-server");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        Pattern ipPatt = Pattern.compile("@(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\b");
+        Pattern domPatt = Pattern.compile("(?!:\\/\\/)([a-zA-Z0-9-_]+\\.)*[a-zA-Z0-9][a-zA-Z0-9-_]+\\.[a-zA-Z]{2,11}?");
+
+        Matcher matchIp = ipPatt.matcher(String.join(" ", cmd.getArgList()));
+        Matcher matchDom = domPatt.matcher(String.join(" ", cmd.getArgList()));
+        
+        if (!matchIp.find() || !matchDom.find() || cmd.getArgList().size() == 0) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("DnsClient", options);
+            throw new Exception("DnsClient requires a valid server IP defined by @a.b.c.d and a valid domain name to query");
+        }
+        
+        if (cmd.hasOption("mx") && !cmd.hasOption("ns")) {
+            queryType = "mx";
+        }
+        else if (cmd.hasOption("ns") && !cmd.hasOption("mx")) {
+            queryType = "ns";
+        }
+        else if (cmd.hasOption("ns") && cmd.hasOption("mx")) {
+            throw new Exception("DnsClient cannot accept both ns and mx options");
+        }
+        else {
+            queryType = DEFAULT_QUERY_TYPE;
+        }
+
+        dnsIp = matchIp.group();
+        domainName = matchDom.group();
+        timeout = cmd.getOptionValue("t", DEFAULT_TIMEOUT);
+        maxEntries = cmd.getOptionValue("r", DEFAULT_MAX_ENTRIES);
+        port = cmd.getOptionValue("p", DEFAULT_PORT);
+
+        optVals.put("timeout", timeout);
+        optVals.put("maxEntries", maxEntries);
+        optVals.put("port", port);
+        optVals.put("queryType", queryType);
+        optVals.put("dnsIp", dnsIp);
+        optVals.put("domainName", domainName);
+
+        return optVals;
     }
 }
--- DnsClient/src/test/java/ca/mcgill/ecse/telecom/DnsClientTest.java
+++ DnsClient/src/test/java/ca/mcgill/ecse/telecom/DnsClientTest.java
@@ -1,16 +1,52 @@
 package ca.mcgill.ecse.telecom;
 
+import org.apache.commons.cli.Options;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Unit test for simple DnsClient.
  */
 public class DnsClientTest {
-    
+
     @Test
-    public void testDnsClient() {
-        assertTrue(true);
+    public void testCommandLine1() {
+        String[] args = {"-t", "10", "-r", "2", "-mx", "@8.8.8.8", "mcgill.ca"};
+        try {
+            HashMap<String, String> pArgs = DnsClient.parseArguments(args);
+        
+            assertEquals("10", pArgs.get("timeout"));
+            assertEquals("2", pArgs.get("maxEntries"));
+            assertEquals("53", pArgs.get("port"));
+            assertEquals("mx", pArgs.get("queryType"));
+            assertEquals("@8.8.8.8", pArgs.get("dnsIp"));
+            assertEquals("mcgill.ca", pArgs.get("domainName"));
+        }
+        catch(Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCommandLineFail1() {
+        String[] args = {"-t", "10", "-r", "2", "-mx", "@8.8.8.8", "mcgill.ca"};
+        try {
+            HashMap<String, String> pArgs = DnsClient.parseArguments(args);
+        
+            assertEquals("10", pArgs.get("timeout"));
+            assertEquals("2", pArgs.get("maxEntries"));
+            assertEquals("53", pArgs.get("port"));
+            assertEquals("mx", pArgs.get("queryType"));
+            assertEquals("@8.8.8.8", pArgs.get("dnsIp"));
+            assertEquals("mcgill.ca", pArgs.get("domainName"));
+        }
+        catch(Exception e) {
+            fail(e.getMessage());
+        } 
     }
 }
