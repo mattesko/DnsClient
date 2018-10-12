787ffa26e023f940f8c42fa591b6c85bbfeeea3b
Matthew Lesko <mlesko1996@gmail.com>
2018-10-11 20:06:17 -0400

    Remove unused methods

--- DnsClient/src/main/java/ca/mcgill/ecse/telecom/DnsClientLogger.java
+++ DnsClient/src/main/java/ca/mcgill/ecse/telecom/DnsClientLogger.java
@@ -64,53 +64,5 @@ public class DnsClientLogger {
         System.out.printf("Server: %s\n", this.requestArgs.get("dnsIp"));
         System.out.printf("Request type: %s\n", this.requestArgs.get("queryType").toUpperCase());
     }
-
-    public void printResponse(DnsPacket dnsPacket, long elapsedTime) {
-        DnsPacket.DnsPacketHeader header = dnsPacket.getPacketHeader();
-        DnsPacket.DnsPacketQuestion question = dnsPacket.getPacketQuestion();
-        DnsPacket.DnsPacketAnswer answer = dnsPacket.getPacketAnswer();
-
-        System.out.printf("Response received after %d seconds (%s retries)", elapsedTime, this.requestArgs.get("maxRetries"));
-        
-        if (header.getAncount() > 0) {
-            System.out.printf("***Answer Section (%d records)", header.getAncount());
-            printAnswerSection(header, question, answer);
-        }
-        else {
-            System.out.println("NOTFOUND");
-        }
-
-        if (header.getArcount() > 0) {
-            System.out.printf("***Additional Section (%d records)", header.getArcount());
-            printAdditionalSection(header, question, answer);
-        }
-
     }
 
-    private void printAnswerSection(DnsPacket.DnsPacketHeader header, DnsPacket.DnsPacketQuestion question, DnsPacket.DnsPacketAnswer answer) {
-        String alias = ""; // TODO Idk what this is
-        String type = answer.getType() == ANSWER_TYPE_A ? "IP" : 
-            answer.getType() == ANSWER_TYPE_NS ? "NS" : 
-            answer.getType() == ANSWER_TYPE_MX ? "MX" : 
-            answer.getType() == ANSWER_TYPE_CNAME ? "CNAME" : "";
-
-        if (answer.getType() == ANSWER_TYPE_A) {
-            
-        }
-        else if (answer.getType() == ANSWER_TYPE_MX) {
-            
-        }
-        else {
-            
-        }
-    }
-
-    /*
-        IP <tab> [ip address] <tab> [seconds can cache] <tab> [auth | nonauth]
-
-        CNAME <tab> [alias] <tab> [seconds can cache] <tab> [auth | nonauth]
-        NS <tab> [alias] <tab> [seconds can cache] <tab> [auth | nonauth]
-
-        MX <tab> [alias] <tab> [pref] <tab> [seconds can cache] <tab> [auth | nonauth]
-    */
-}
