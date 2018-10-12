2ea0074b23da4864ecf6486177ca1e36b7edd8c0
Matthew Lesko <mlesko1996@gmail.com>
2018-10-11 20:14:27 -0400

    Print answer records

--- DnsClient/src/main/java/ca/mcgill/ecse/telecom/DnsClientLogger.java
+++ DnsClient/src/main/java/ca/mcgill/ecse/telecom/DnsClientLogger.java
@@ -19,11 +19,79 @@ public class DnsClientLogger {
         this.requestArgs = requestArgs;
     }
 
-    public void PrintPacket(DatagramPacket dgPacket) {
+    public void printResponse(DatagramPacket dgPacket, DnsPacket packetModel, long elapsedTime) throws Exception{
+        ByteBuffer buffer = ByteBuffer.wrap(dgPacket.getData());
+
+        byte[] dataHeader = new byte[12];
+        buffer.get(dataHeader, 0, 12);
+        parseResponseHeader(ByteBuffer.wrap(dataHeader), packetModel.getPacketHeader().getId());
+
+        // Bytes taken up by the QNAME is dynamic, but QTYPE and QCLASS take up 4 bytes and need 2 additional bytes for the first and last labels
+        byte[] dataQuestion = new byte[requestArgs.get("domainName").length() + 2 + 4];
+        buffer.get(dataQuestion, 0, dataQuestion.length);
+        parseResponseQuestion(ByteBuffer.wrap(dataQuestion));
+
+        byte[] dataAnswer = new byte[DnsClient.DATAGRAM_RESPONSE_SIZE - dataHeader.length - dataQuestion.length];
+        buffer.get(dataAnswer, 0, dataAnswer.length);
+        printAnswerRecords(ByteBuffer.wrap(dataAnswer), packetModel);
+    }
+
+    private void printAnswerRecords(ByteBuffer answer, DnsPacket packetModel) {
+
+        while (answer.remaining() > 0) {
+
+            short name = answer.getShort();
+            short type = answer.getShort();
+            short class_x = answer.getShort();
+            int timeToLive = answer.getInt();
+            short dataLength = answer.getShort();
+            byte[] data = new byte[dataLength];
+
+            if (type == ANSWER_TYPE_A) {
+                answer.get(data, 0, dataLength);
+                System.out.printf("IP \t %d.%d.%d.%d \t %d \t %s",
+                    data[0] > 0 ? data[0] : data[0] + 255,
+                    data[1] > 0 ? data[1] : data[1] + 255,
+                    data[2] > 0 ? data[2] : data[2] + 255,
+                    data[3] > 0 ? data[3] : data[3] + 255,
+                    timeToLive,
+                    "auth | nonauth"
+                );
+            }
+            else if (type == ANSWER_TYPE_NS) {
+                answer.get(data, 0, dataLength);
+                System.out.printf("NS \t %s \t %d \t %s", 
+                    new String(data, StandardCharsets.UTF_8),
+                    timeToLive,
+                    "auth | nonauth"
+                );
+            }
+            else if (type == ANSWER_TYPE_CNAME) {
+                answer.get(data, 0, dataLength);
+                System.out.printf("CNAME \t %s \t %d \t %s", 
+                    new String(data, StandardCharsets.UTF_8),
+                    timeToLive,
+                    "auth | nonauth"
+                );
+            }
+            else if (type == ANSWER_TYPE_MX) {
+                short preference = answer.getShort();
+                answer.get(data, 0, dataLength - 2);
+                System.out.printf("MX \t %s \t %d \t %d \t %s", 
+                    new String(data, StandardCharsets.UTF_8),
+                    preference,
+                    timeToLive,
+                    "auth | nonauth"
+                );
+            }
+        }
+    }
         
+    private void parseResponseQuestion(ByteBuffer question) {
 
 
     }
+
     //Parse the header and print the result (For Now)
     private static void parseResponseHeader(ByteBuffer Header, int ID) throws Exception{
         //Check For Matching ID - 2 bytes
